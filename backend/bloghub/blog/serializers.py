a2889ac128ae3c55946a2db6d45581405f342a43
Aleksandra Zamurueva <alexandra42kuz@gmail.com>
2025-03-18 11:07:13 +0700

    Изменения в регистрации для улучшения БД

--- backend/bloghub/blog/serializers.py
+++ backend/bloghub/blog/serializers.py
@@ -1,13 +1,14 @@
-# blog/serializers.py
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
         extra_kwargs = {
             'password': {'write_only': True},
         }
@@ -18,9 +19,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
+        validated_data.pop('password2')  
+        user = User.objects.create_user(**validated_data)
         return user
--- backend/bloghub/blog/views.py
+++ backend/bloghub/blog/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer  # Убедитесь, что у вас есть сериализатор для регистрации
+from .serializers import RegisterSerializer 
 
 class RegisterView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            # Логика регистрации
             serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
