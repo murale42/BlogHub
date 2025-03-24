a9d9210eecf89cf37eb0691b4735d5458f1402f8
Elizaveta Klyushneva <elizaveta.kly2004@mail.ru>
2025-03-24 18:07:55 +0700

    Создала страницу PasswordResetForm.vue

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <HeaderComponent />
+    
+    <main>
+      <div class="container py-5">
+        <!-- Пока нет контента на главной странице -->
+      </div>
+    </main>
+
+    <FooterComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from './Header.vue';
+import FooterComponent from './Footer.vue';
+
 export default {
-  name: 'HomePage'
+  components: {
+    HeaderComponent,
+    FooterComponent
+  }
 };
 </script>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  text-align: center;
+  width: 100%;
+}
+
+footer {
+  margin-top: auto;
+}
 </style>
