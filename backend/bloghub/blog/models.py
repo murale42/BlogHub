cd77d222a636c52545b1450a80953fd1baa81e20
Aleksandra Zamurueva <alexandra42kuz@gmail.com>
2025-03-26 12:03:31 +0700

    Добавила комментарии для удобства работы в админке

--- backend/bloghub/blog/models.py
+++ backend/bloghub/blog/models.py
@@ -11,19 +11,27 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name='Заголовок')
+    slug = models.SlugField(unique=True,
+                            verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL;'
+                                      ' разрешены символы латиницы, цифры,'
+                                      ' дефис и подчёркивание.')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
+    content = models.TextField(verbose_name='Текст')
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     video = models.FileField(upload_to='post_videos/', blank=True, null=True)
-    categories = models.ManyToManyField(Category, related_name='posts')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    categories = models.ManyToManyField(Category, related_name='posts', verbose_name='Категория')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор публикации')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
